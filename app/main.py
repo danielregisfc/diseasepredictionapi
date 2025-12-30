5c5a4b601dd571c32afe6150fb3dc7c1c02a52f3
Daniel Régis <danielregisfc@gmail.com>
2025-12-29 22:04:50 -0300

    Rota Post api predict

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,24 @@ def service():
     return jsonify({
         "status":"ok",
         "service":"disease-prediction-api"
+    })
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    required_fields = ["age", "blood_pressure", "cholesterol"]
+
+    if not data:
+        return jsonify({"error": "JSON inválido ou vazio"}), 400
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"error": f"Campo obrigatório ausente: {field}"}), 400
+
+    return jsonify({
+        "message": "Dados válidos recebidos",
+        "input": data
     })
 
 if __name__ == "__main__":
